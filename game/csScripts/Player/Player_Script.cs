4abb299a73495163894640ed4be3f5c0ec3f0009
nestharus <mrasolomon@gmail.com>
2014-12-02 21:35:49 -0800

    fixed references

--- game/csScripts/Player/Player_Script.cs
+++ game/csScripts/Player/Player_Script.cs
@@ -4,26 +4,26 @@ using WinterLeaf.Engine.Classes;
 using WinterLeaf.Engine.Classes.Interopt;
 
 static public class Player_Script
-    {
-    public static readonly pInvokes omni = new pInvokes();
-    static public void playPain(Player player)
-        {
-        player.playAudio(0, "PainCrySound");
-        omni.console.error("The script is doing this!");
+{
+	public static readonly pInvokes omni = new pInvokes();
+	static public void playPain(Player player)
+	{
+		player.playAudio(0, "PainCrySound");
+		omni.console.error("The script is doing this!");
 
-        }
-    static public void playDeathCry(Player player)
-        {
-        player.playAudio(0, "DeathCrySound");
-        }
+	}
+	static public void playDeathCry(Player player)
+	{
+		player.playAudio(0, "DeathCrySound");
+	}
 
-    static public void DoSomethingcool(Player player,string myMessage)
-        {
-        omni.console.error("This is something cool " + myMessage);
-        }
-    }
+	static public void DoSomethingcool(Player player, string myMessage)
+	{
+		omni.console.error("This is something cool " + myMessage);
+	}
+}
 
- 
+
 
 
 
