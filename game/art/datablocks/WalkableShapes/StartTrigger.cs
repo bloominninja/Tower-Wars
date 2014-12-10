ccceb4369a77569219ba31c82fdb632b89806bfb
nestharus <mrasolomon@gmail.com>
2014-12-10 11:03:21 -0800

    aa

--- game/art/datablocks/WalkableShapes/StartTrigger.cs
+++
@@ -1,9 +0,0 @@
-//-----------------------------------------------------------------------------
-// StartTrigger is located at the player spawn point so we can give them some 
-// instructions the first time they spawn.
-//-----------------------------------------------------------------------------
-datablock TriggerData(StartTrigger : DefaultTrigger)
-{
-   tickPeriodMS = 500;
-   WLE_OVERRIDE_PROXY_CLASSTYPE = "LaughingDogStudios.Salvage.Logic.Models.User.GameCode.Server.WalkableShape.StartTrigger";
-};
